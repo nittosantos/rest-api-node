12b811c7b88b697cb291ce0b9359a89c53072df5
Nitto <nittosantos.dev@gmail.com>
2024-05-07 12:10:53 -0300

    feat: :sparkles: config env

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,20 @@
+import "dotenv/config";
 import { knex as setupKnex, Knex } from "knex";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("not found database url");
+}
 
 export const config: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: "./db/app.db",
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
-    extension: 'ts',
-    directory: './db/migrations'
-  }
+    extension: "ts",
+    directory: "./db/migrations",
+  },
 };
 
 export const knex = setupKnex(config);
